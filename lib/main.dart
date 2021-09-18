7d5338a9afe0530fb8c50aeee13d1de907106e26
Drakunal <drakunaldey1@gmail.com>
2021-09-18 21:27:02 +0530

    firebase and core upgrade in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:brew_crew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
-      home: Wrapper(),
-    ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Wrapper());
+}
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,4 +1,6 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -8,6 +10,8 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,9 +21,21 @@ class _SignInState extends State<SignIn> {
         elevation: 0,
         title: Text("Brew Crew"),
       ),
-      body: Container(
-        child: ElevatedButton(
-            onPressed: () async {}, child: Text("Sign in anonymously")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          child: ElevatedButton(
+              onPressed: () async {
+                dynamic result = await _auth.signInAnon();
+                if (result == null) {
+                  print("error signing in...");
+                } else {
+                  print("Signed in...");
+                  print("Result");
+                }
+              },
+              child: Text("Sign in anonymously")),
+        ),
       ),
     );
   }
