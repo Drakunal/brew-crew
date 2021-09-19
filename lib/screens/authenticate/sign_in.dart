c73e3edcc8b84a49b677e9913bde812e8cc00110
Drakunal <drakunaldey1@gmail.com>
2021-09-19 19:51:40 +0530

    ui updated

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -27,24 +27,40 @@ class _SignInState extends State<SignIn> {
           child: Form(
               child: Column(
             children: [
+              SizedBox(height: 100),
               TextFormField(
                 onChanged: (value) {},
               ),
+              SizedBox(height: 50),
               TextFormField(
                 onChanged: (value) {},
                 obscureText: true,
               ),
-              ElevatedButton(
-                  onPressed: () async {
-                    dynamic result = await _auth.signInAnon();
-                    if (result == null) {
-                      print("error signing in...");
-                    } else {
-                      print("Signed in...");
-                      print(result.uId);
-                    }
-                  },
-                  child: Text("Sign in anonymously")),
+              SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          dynamic result = await _auth.signInAnon();
+                          if (result == null) {
+                            print("error signing in...");
+                          } else {
+                            print("Signed in...");
+                            print(result.uId);
+                          }
+                        },
+                        child: Text("Sign in anonymously")),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: ElevatedButton(
+                        onPressed: () async {}, child: Text("Sign in")),
+                  )
+                ],
+              ),
             ],
           ))
           // child: Container(
