dcf32354770965127815c3333045cb8a0fa4490e
Drakunal <drakunaldey1@gmail.com>
2021-09-20 12:21:25 +0530

    loading screen implemented

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constant.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'authenticate.dart';
@@ -18,128 +19,136 @@ class _RegisterState extends State<Register> {
   String _email = '';
   String _password = '';
   String _error = '';
+  bool loading = false;
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown.shade100,
-      appBar: AppBar(
-        backgroundColor: Colors.brown.shade400,
-        elevation: 0,
-        title: Text("Register to Brew Crew"),
-        actions: [
-          TextButton.icon(
-              style: ElevatedButton.styleFrom(
-                  // primary: Colors.white,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown.shade100,
+            appBar: AppBar(
+              backgroundColor: Colors.brown.shade400,
+              elevation: 0,
+              title: Text("Register to Brew Crew"),
+              actions: [
+                TextButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        // primary: Colors.white,
 
-                  ),
-              onPressed: () {
-                widget.toggleFunctionName();
-                print("pressed");
-              },
-              icon: Icon(Icons.login, color: Colors.white),
-              label: Text(" Login",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold))),
-        ],
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SizedBox(height: 50),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                      hintText: 'Email',
-                    ),
-                    validator: (value) =>
-                        value!.isEmpty ? 'Enter an email address' : null,
-                    onChanged: (value) {
-                      setState(() {
-                        _email = value;
-                      });
+                        ),
+                    onPressed: () {
+                      widget.toggleFunctionName();
+                      print("pressed");
                     },
-                  ),
-                  SizedBox(height: 25),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                      hintText: 'Password',
-                    ),
-                    validator: (value) => value!.length < 6
-                        ? 'Enter a password greater than 6 characters'
-                        : null,
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value;
-                      });
-                    },
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      // Padding(
-                      //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      //   child: ElevatedButton(
-                      //       onPressed: () async {
-                      //         dynamic result = await _auth.signInAnon();
-                      //         if (result == null) {
-                      //           print("error signing in...");
-                      //         } else {
-                      //           print("Signed in...");
-                      //           print(result.uId);
-                      //         }
-                      //       },
-                      //       child: Text("Sign in anonymously")),
-                      // ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                dynamic result =
-                                    _auth.registerWithEmailAndPassword(
-                                        _email, _password);
-                                // if (result == null) {
-                                setState(() {
-                                  _error = 'Please enter a valid email.';
-                                });
-                                // }
-                                print(
-                                    "Email is $_email and the password is $_password");
-                              }
-                            },
-                            child: Text(
-                              "Register",
-                            )),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(_error,
-                      style: TextStyle(
-                          color: Colors.red, fontWeight: FontWeight.bold)),
-                ],
-              ))
-          // child: Container(
-          //   child: ElevatedButton(
-          //       onPressed: () async {
-          //         dynamic result = await _auth.signInAnon();
-          //         if (result == null) {
-          //           print("error signing in...");
-          //         } else {
-          //           print("Signed in...");
-          //           print(result.uId);
-          //         }
-          //       },
-          //       child: Text("Sign in anonymously")),
-          // ),
-          ),
-    );
+                    icon: Icon(Icons.login, color: Colors.white),
+                    label: Text(" Login",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold))),
+              ],
+            ),
+            body: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 50),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Email',
+                          ),
+                          validator: (value) =>
+                              value!.isEmpty ? 'Enter an email address' : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _email = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 25),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Password',
+                          ),
+                          validator: (value) => value!.length < 6
+                              ? 'Enter a password greater than 6 characters'
+                              : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _password = value;
+                            });
+                          },
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            // Padding(
+                            //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            //   child: ElevatedButton(
+                            //       onPressed: () async {
+                            //         dynamic result = await _auth.signInAnon();
+                            //         if (result == null) {
+                            //           print("error signing in...");
+                            //         } else {
+                            //           print("Signed in...");
+                            //           print(result.uId);
+                            //         }
+                            //       },
+                            //       child: Text("Sign in anonymously")),
+                            // ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(() {
+                                        loading = true;
+                                      });
+                                      dynamic result =
+                                          _auth.registerWithEmailAndPassword(
+                                              _email, _password);
+                                      // if (result == null) {
+                                      setState(() {
+                                        _error = 'Please enter a valid email.';
+                                      });
+                                      // }
+                                      print(
+                                          "Email is $_email and the password is $_password");
+                                    }
+                                  },
+                                  child: Text(
+                                    "Register",
+                                  )),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(_error,
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ))
+                // child: Container(
+                //   child: ElevatedButton(
+                //       onPressed: () async {
+                //         dynamic result = await _auth.signInAnon();
+                //         if (result == null) {
+                //           print("error signing in...");
+                //         } else {
+                //           print("Signed in...");
+                //           print(result.uId);
+                //         }
+                //       },
+                //       child: Text("Sign in anonymously")),
+                // ),
+                ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,5 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
 import 'package:brew_crew/shared/constant.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:brew_crew/models/user.dart' as u;
@@ -18,126 +19,139 @@ class _SignInState extends State<SignIn> {
   String _password = '';
   final _formKey = GlobalKey<FormState>();
   String _error = '';
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown.shade100,
-      appBar: AppBar(
-        backgroundColor: Colors.brown.shade400,
-        elevation: 0,
-        title: Text("Brew Crew"),
-        actions: [
-          TextButton.icon(
-              style: ElevatedButton.styleFrom(
-                  // primary: Colors.white,
+    return loading
+        ? Loading()
+        : Scaffold(
+            backgroundColor: Colors.brown.shade100,
+            appBar: AppBar(
+              backgroundColor: Colors.brown.shade400,
+              elevation: 0,
+              title: Text("Brew Crew"),
+              actions: [
+                TextButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        // primary: Colors.white,
 
-                  ),
-              onPressed: () {
-                widget.toggleFunctionName();
-              },
-              icon: Icon(Icons.login, color: Colors.white),
-              label: Text(" Register",
-                  style: TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold))),
-        ],
-      ),
-      body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  SizedBox(height: 50),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                      hintText: 'Email',
-                    ),
-                    validator: (value) =>
-                        value!.isEmpty ? 'Enter an email' : null,
-                    onChanged: (value) {
-                      setState(() {
-                        _email = value;
-                      });
+                        ),
+                    onPressed: () {
+                      widget.toggleFunctionName();
                     },
-                  ),
-                  SizedBox(height: 25),
-                  TextFormField(
-                    decoration: textInputDecoration.copyWith(
-                      hintText: 'Password',
-                    ),
-                    validator: (value) => value!.length < 6
-                        ? 'Enter a password of atleast 6 characters'
-                        : null,
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value;
-                      });
-                    },
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              dynamic result = await _auth.signInAnon();
-                              if (result == null) {
-                                print("error signing in...");
-                              } else {
-                                print("Signed in...");
-                                print(result.uId);
-                              }
-                            },
-                            child: Text("Sign in anonymously")),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: ElevatedButton(
-                            onPressed: () async {
-                              if (_formKey.currentState!.validate()) {
-                                print("valid");
-                                dynamic result =
-                                    _auth.loginWithEmailAndPassword(
-                                        _email, _password);
-                                // if (result == null) {
-                                setState(() {
-                                  _error = 'Wrong Credentials';
-                                });
-                                // }
-                                // print(
-                                //     "Email is $_email and the password is $_password");
-                              }
-                            },
-                            child: Text("Sign in")),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(_error,
-                      style: TextStyle(
-                          color: Colors.red, fontWeight: FontWeight.bold)),
-                ],
-              ))
-          // child: Container(
-          //   child: ElevatedButton(
-          //       onPressed: () async {
-          //         dynamic result = await _auth.signInAnon();
-          //         if (result == null) {
-          //           print("error signing in...");
-          //         } else {
-          //           print("Signed in...");
-          //           print(result.uId);
-          //         }
-          //       },
-          //       child: Text("Sign in anonymously")),
-          // ),
-          ),
-    );
+                    icon: Icon(Icons.login, color: Colors.white),
+                    label: Text(" Register",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold))),
+              ],
+            ),
+            body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        SizedBox(height: 50),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Email',
+                          ),
+                          validator: (value) =>
+                              value!.isEmpty ? 'Enter an email' : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _email = value;
+                            });
+                          },
+                        ),
+                        SizedBox(height: 25),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                            hintText: 'Password',
+                          ),
+                          validator: (value) => value!.length < 6
+                              ? 'Enter a password of atleast 6 characters'
+                              : null,
+                          onChanged: (value) {
+                            setState(() {
+                              _password = value;
+                            });
+                          },
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    setState(() {
+                                      loading = true;
+                                    });
+                                    dynamic result = await _auth.signInAnon();
+                                    if (result == null) {
+                                      print("error signing in...");
+                                    } else {
+                                      print("Signed in...");
+                                      print(result.uId);
+                                    }
+                                  },
+                                  child: Text("Sign in anonymously")),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8.0),
+                              child: ElevatedButton(
+                                  onPressed: () async {
+                                    if (_formKey.currentState!.validate()) {
+                                      setState(() {
+                                        loading = true;
+                                      });
+                                      print("valid");
+                                      dynamic result =
+                                          _auth.loginWithEmailAndPassword(
+                                              _email, _password);
+                                      // if (result == null) {
+                                      setState(() {
+                                        // loading = false;
+                                        _error = 'Wrong Credentials';
+                                      });
+                                      // }
+                                      // print(
+                                      //     "Email is $_email and the password is $_password");
+                                    }
+                                  },
+                                  child: Text("Sign in")),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 12,
+                        ),
+                        Text(_error,
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold)),
+                      ],
+                    ))
+                // child: Container(
+                //   child: ElevatedButton(
+                //       onPressed: () async {
+                //         dynamic result = await _auth.signInAnon();
+                //         if (result == null) {
+                //           print("error signing in...");
+                //         } else {
+                //           print("Signed in...");
+                //           print(result.uId);
+                //         }
+                //       },
+                //       child: Text("Sign in anonymously")),
+                // ),
+                ),
+          );
   }
 }
