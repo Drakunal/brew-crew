a6655708d8e9aadeafe2379c5edbd8f0acbbd24a
Drakunal <drakunaldey1@gmail.com>
2021-09-20 17:37:35 +0530

    data read from fire store in list:

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -12,11 +12,49 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot?>(context);
+    final brews = Provider.of<QuerySnapshot>(context);
     // print(brews!.docs);
-    for (var doc in brews!.docs) {
-      print(doc.data());
-    }
-    return Container();
+    // for (var doc in brews!.docs) {
+    //   print(doc.data());
+    // }
+
+    return ListView.builder(
+        itemCount: brews.size,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 4.5,
+            shadowColor: Colors.black,
+            color: Colors.white,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.brown,
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      // image: DecorationImage(
+                      //     image: NetworkImage(movieList[index].images[0]),
+                      //     fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: null,
+                ),
+              ),
+              title: Text(brews.docs[index]['strength'].toString()),
+              // subtitle: Text(movieList[index].director),
+              // trailing: const Text("..."),
+              onTap: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => MovieListViewDetails(
+                //               movieName: movieList.elementAt(index).title,
+                //               movie: movieList[index],
+                //             )));
+              },
+              // onTap: () =>
+              //     debugPrint("The movie name is ${movies.elementAt(index)}"),
+            ),
+          );
+        });
   }
 }
---
+++ lib/screens/home/brew_tile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BrewTile extends StatelessWidget {
+  const BrewTile({Key? key}) : super(key: key);
+  // final Brew brew;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
