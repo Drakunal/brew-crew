62918ef582957ac2bbb3dc58e31914eecd8ce815
Drakunal <drakunaldey1@gmail.com>
2021-09-21 09:59:20 +0530

    model data successfully accessed in list

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -18,53 +18,57 @@ class _BrewListState extends State<BrewList> {
     // for (var doc in brews!.docs) {
     //   print(doc.data());
     // }
+    int len;
     if (brews != null) {
+      len = brews.length;
       brews.forEach((brew) {
         print(brew.name);
         print(brew.sugar);
         print(brew.strength);
       });
+    } else {
+      len = 0;
     }
-    return Container();
-    //   return ListView.builder(
-    //       itemCount: brews.length,
-    //       itemBuilder: (context, index) {
-    //         return Card(
-    //           elevation: 4.5,
-    //           shadowColor: Colors.black,
-    //           color: Colors.white,
-    //           child: ListTile(
-    //             leading: CircleAvatar(
-    //               backgroundColor: Colors.brown,
-    //               child: Container(
-    //                 width: 200,
-    //                 height: 200,
-    //                 decoration: BoxDecoration(
-    //                     // image: DecorationImage(
-    //                     //     image: NetworkImage(movieList[index].images[0]),
-    //                     //     fit: BoxFit.cover),
-    //                     borderRadius: BorderRadius.circular(20)),
-    //                 child: null,
-    //               ),
-    //             ),
-    //             title: Text("Hi"),
-    //             // title: Text(brews.docs[index]['strength'].toString()),
-    //             // subtitle: Text(movieList[index].director),
-    //             // trailing: const Text("..."),
-    //             onTap: () {
-    //               // Navigator.push(
-    //               //     context,
-    //               //     MaterialPageRoute(
-    //               //         builder: (context) => MovieListViewDetails(
-    //               //               movieName: movieList.elementAt(index).title,
-    //               //               movie: movieList[index],
-    //               //             )));
-    //             },
-    //             // onTap: () =>
-    //             //     debugPrint("The movie name is ${movies.elementAt(index)}"),
-    //           ),
-    //         );
-    //       });
-    // }
+    // return Container();
+
+    return ListView.builder(
+        itemCount: len,
+        itemBuilder: (context, index) {
+          return Card(
+            elevation: 4.5,
+            shadowColor: Colors.black,
+            color: Colors.white,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.brown[brews![index].strength],
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                      // image: DecorationImage(
+                      //     image: NetworkImage(movieList[index].images[0]),
+                      //     fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: null,
+                ),
+              ),
+              title: Text(brews![index].name),
+              // title: Text(brews.docs[index]['strength'].toString()),
+              subtitle: Text("Takes ${brews![index].sugar} sugar"),
+              // trailing: const Text("..."),
+              onTap: () {
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => MovieListViewDetails(
+                //               movieName: movieList.elementAt(index).title,
+                //               movie: movieList[index],
+                //             )));
+              },
+              // onTap: () =>
+              //     debugPrint("The movie name is ${movies.elementAt(index)}"),
+            ),
+          );
+        });
   }
 }
