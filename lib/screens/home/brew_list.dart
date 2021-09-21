efcd296f873c7f29e54b66cca7dfb9a66b07a732
Drakunal <drakunaldey1@gmail.com>
2021-09-21 09:51:21 +0530

    brew model implemented

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -1,3 +1,4 @@
+import 'package:brew_crew/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
@@ -12,49 +13,58 @@ class BrewList extends StatefulWidget {
 class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
-    final brews = Provider.of<QuerySnapshot>(context);
+    final brews = Provider.of<List<Brew>?>(context);
     // print(brews!.docs);
     // for (var doc in brews!.docs) {
     //   print(doc.data());
     // }
-
-    return ListView.builder(
-        itemCount: brews.size,
-        itemBuilder: (context, index) {
-          return Card(
-            elevation: 4.5,
-            shadowColor: Colors.black,
-            color: Colors.white,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.brown,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                      // image: DecorationImage(
-                      //     image: NetworkImage(movieList[index].images[0]),
-                      //     fit: BoxFit.cover),
-                      borderRadius: BorderRadius.circular(20)),
-                  child: null,
-                ),
-              ),
-              title: Text(brews.docs[index]['strength'].toString()),
-              // subtitle: Text(movieList[index].director),
-              // trailing: const Text("..."),
-              onTap: () {
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //         builder: (context) => MovieListViewDetails(
-                //               movieName: movieList.elementAt(index).title,
-                //               movie: movieList[index],
-                //             )));
-              },
-              // onTap: () =>
-              //     debugPrint("The movie name is ${movies.elementAt(index)}"),
-            ),
-          );
-        });
+    if (brews != null) {
+      brews.forEach((brew) {
+        print(brew.name);
+        print(brew.sugar);
+        print(brew.strength);
+      });
+    }
+    return Container();
+    //   return ListView.builder(
+    //       itemCount: brews.length,
+    //       itemBuilder: (context, index) {
+    //         return Card(
+    //           elevation: 4.5,
+    //           shadowColor: Colors.black,
+    //           color: Colors.white,
+    //           child: ListTile(
+    //             leading: CircleAvatar(
+    //               backgroundColor: Colors.brown,
+    //               child: Container(
+    //                 width: 200,
+    //                 height: 200,
+    //                 decoration: BoxDecoration(
+    //                     // image: DecorationImage(
+    //                     //     image: NetworkImage(movieList[index].images[0]),
+    //                     //     fit: BoxFit.cover),
+    //                     borderRadius: BorderRadius.circular(20)),
+    //                 child: null,
+    //               ),
+    //             ),
+    //             title: Text("Hi"),
+    //             // title: Text(brews.docs[index]['strength'].toString()),
+    //             // subtitle: Text(movieList[index].director),
+    //             // trailing: const Text("..."),
+    //             onTap: () {
+    //               // Navigator.push(
+    //               //     context,
+    //               //     MaterialPageRoute(
+    //               //         builder: (context) => MovieListViewDetails(
+    //               //               movieName: movieList.elementAt(index).title,
+    //               //               movie: movieList[index],
+    //               //             )));
+    //             },
+    //             // onTap: () =>
+    //             //     debugPrint("The movie name is ${movies.elementAt(index)}"),
+    //           ),
+    //         );
+    //       });
+    // }
   }
 }
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:brew_crew/models/brew.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
